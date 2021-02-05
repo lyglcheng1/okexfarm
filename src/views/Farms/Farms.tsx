ff45ebad86d62416a0e6cfbb4a03e24440ba9f7e
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-02-05 22:19:30 +0530

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -27,7 +27,7 @@ const Farms: React.FC = () => {
             <Route exact path={path}>
               <PageHeader
                 icon={<img src={chef} height="120" />}
-                subtitle="Earn OKF tokens by staking OkfSwap V2 SLP Tokens."
+                subtitle="Earn OKF tokens by staking uniswap V2 LP Tokens."
                 title="Select Your Favorite Dishes"
               />
               <div
