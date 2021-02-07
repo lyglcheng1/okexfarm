60b66d8c30c516b0ca8a7084111002ce314a1636
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-02-08 00:36:50 +0530

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,3 @@
-export const okf = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
-export const okfv2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
-export const okfAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
-export const masterChefAddress = '0x245A074cA9814fB46A21562bC70fAB92F8A3F779'
+export const okf = '0xB7fD921e15904645Bc7f5308f3e6a843080928B3'
+export const okfbar = '0xcf6cCC6a81138069a36C3A604595e8cFA617fd2c'
+export const masterChefAddress = '0xaf7600DF103A2E1C2a707E233Bd2A51Ad63c12f6'
