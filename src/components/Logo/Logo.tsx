328d6455254760f35532f3504fb16531b48d9d52
OKEx Farm <77991984+okexfarm@users.noreply.github.com>
2021-02-18 02:31:20 +0530

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -8,7 +8,7 @@ const Logo: React.FC = () => {
     <StyledLogo to="/">
       <img src={chef} height="32" style={{ marginTop: -4 }} />
       <StyledText>
-        Kswap <MasterChefText> OKF FARM</MasterChefText>
+        Kswap <MasterChefText> OKF Farm</MasterChefText>
       </StyledText>
     </StyledLogo>
   )
